1bed3a0357c49ebd2a0b0e5c2da7ffb22b62fd33
beingaayush <beingaayushraj@gmail.com>
2026-07-17 16:24:27 +0530

    day 3 - Tokens, Tokenization, Tokenizer, input/output tokens, Lego Block analogy, english VS hinglish VS hindi tokens, why output tokens costs more?

---
+++ week1/day3/tokens.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+# Load API key
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+# Model
+model = "llama-3.3-70b-versatile"
+
+# Prompts
+prompts = [
+    "Hi",
+    "What is DBMS?",
+    "Write a 1000-word essay on Machine Learning."
+]
+
+# Loop through prompts
+for prompt in prompts:
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    print(f"\nPrompt: {prompt}")
+    # print(response.choices[0].message.content)
+    print(f"Input: {response.usage.prompt_tokens} | Output: {response.usage.completion_tokens} | Total: {response.usage.total_tokens}")
