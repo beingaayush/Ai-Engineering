710333156c7272705d0580f7dcc4a5e11d2ffd79
beingaayush <beingaayushraj@gmail.com>
2026-07-19 22:26:43 +0530

    code updated- pathlib removed

--- week1/day1/LLM_CALL.py
+++ week1/day1/LLM_CALL.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 from  dotenv import load_dotenv
 from groq import Groq
 
@@ -12,18 +11,16 @@ if not my_api_key:
 client=Groq(api_key=my_api_key)    
 
 model="llama-3.3-70b-versatile"
-role="user"
 prompt="do you know elon musk ?"
-message={
-    "role": role,
-    "content": prompt
-}
-
-messages=[message]
 
 response=client.chat.completions.create(
     model=model,
-    messages=messages
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
 )
 
 print(response.choices[0].message.content)
