fec6a263078c83016e2d3384f9daeea44a0f1a1b
beingaayush <beingaayushraj@gmail.com>
2026-08-08 23:31:58 +0530

    week3 - intro to RAG

---
+++ week3/day11/RAG_INTRO.py
@@ -0,0 +1,12 @@
+import os
+from  dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)    
+model="llama-3.3-70b-versatile"
